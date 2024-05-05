c0ba1cc3c6c413240a8371dc79b109207828edbb
MayraLeticia <mayraleticia03@gmail.com>
2024-05-04 21:19:02 -0300

    subindo projeto para o github
    :wq

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import "./style.scss";
+import Logo from "../../assets/images/Logo.svg";
+import Crown from "../../assets/icons/Crown.svg";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components";
+
+
+const Header = ({  }) => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="header">
+            <img src={Logo} alt="Logo" />
+            <div className="header-buttons">
+            <Button 
+            variant="button-filled-green"
+            name="Teste grátis"
+            onClick={() => {
+                navigate("/#");
+                //fazer pop up de cadastro parcial de email
+            }}
+            />
+            <Button 
+            variant="button-border-red"
+            name="Login"
+            icon={Crown}
+            onClick={() => {navigate("/login")}}
+            />
+            </div>
+        </div>
+    );
+}
+
+
+export default Header;
---
+++ src/screens/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.scss";
+import { } from "../../components";
+
+
+const Login = () => {
+    return (
+        <div>
+
+
+        </div>
+    );
+}
+
+
+export default Login;
